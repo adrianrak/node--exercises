632378c671b6f5bed1fd0626fed160419a051f80
Adrian Rak <adrrak@tlen.pl>
2017-12-05 14:45:15 +0100

    colors

--- modules/OSInfo.js
+++ modules/OSInfo.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var colors = require('colors');
 var time = require('./timeChange');
 
 function getOSinfo() {
@@ -12,12 +13,12 @@ function getOSinfo() {
     var cpu = os.cpus()[0].model;
     var uptime = os.uptime();
     var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ' + time.timeChange(uptime));
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
+    console.log('System:'.green, type);
+    console.log('Release:'.red, release);
+    console.log('CPU model:'.blue, cpu);
+    console.log('Uptime: '.yellow + time.timeChange(uptime));
+    console.log('User name:'.magenta, userInfo.username);
+    console.log('Home dir:'.grey, userInfo.homedir);
 }
 
 exports.print = getOSinfo;
