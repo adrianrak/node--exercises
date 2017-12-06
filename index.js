19413b171061a785a6745644b9271456ffdf4ede
Adrian Rak <adrrak@tlen.pl>
2017-12-06 10:22:11 +0100

    loop for - data[i]

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ var colors = require('colors');
 fs.readdir('./catalog', 'utf-8', function(err, data) {
   if (err) throw err;
   console.log('Loaded!'.red);
+  for (var i = 0 ; i < data.length ; i++) { 
+    console.log(data[i]);
+    data[i] = '\n' + (i + 1) + '. ' + data[i];
+  }
   fs.writeFile('./catalog_text.txt', `catalog: ${data}`, function(err) {
     if (err) throw err;
     console.log('Saved!'.blue);
   });
+
 });
 // var fs = require('fs');
 // var StatMode = require('stat-mode');
